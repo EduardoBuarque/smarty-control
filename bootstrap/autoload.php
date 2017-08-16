47c9fc4a21b767a43e0fe0f90bd7fee8e22076be
Alécio Santana da Silva <aleciosantana@gmail.com>
2017-08-15 21:46:02 -0300

    removendo besteiras

--- bootstrap/autoload.php
+++ bootstrap/autoload.php
@@ -4,7 +4,7 @@ define('LARAVEL_START', microtime(true));
 
 /*
 |--------------------------------------------------------------------------
-| Register The Composer Auto Loader 
+| Register The Composer Auto Loader
 |--------------------------------------------------------------------------
 |
 | Composer provides a convenient, automatically generated class loader
