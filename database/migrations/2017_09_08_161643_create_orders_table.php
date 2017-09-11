e08bdb85223257672717eda702b8ca84f2a0b25c
Alécio Santana da Silva <aleciosantana@gmail.com>
2017-09-11 18:17:20 -0300

    migrations orders e order_product

---
+++ database/migrations/2017_09_08_161643_create_orders_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamp('date');
+            $table->string('status', 1)->default('A');
+            $table->integer('customer_id')->unsigned();
+            $table->integer('user_id')->unsigned();
+        });
+        Schema::table('orders', function (Blueprint $table) {
+            $table->foreign('customer_id')->references('id')->on('customers')->onDelete('cascade');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
